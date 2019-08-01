2e439282360b885fa8a5a4e5a0aea0c14533dc0c
Jason <4jasonmann@gmail.com>
2019-08-01 13:08:26 +0200

    dash

---
+++ Assets/Scripts/dashSkill.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dashSkill : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float dashSpeed;
+    private float dashTime;
+    public float startDashTime;
+    private int direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        dashTime = startDashTime;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+           
+                if (dashTime <= 0)
+                {
+                    direction = 0;
+                    dashTime = startDashTime;
+                    rb.velocity = Vector2.zero;
+                }
+                else
+                {
+                    dashTime -= Time.deltaTime;
+                }
+            
+
+            
+            
+            {
+                rb.AddForce(new Vector2(10000f, 0f));
+            }
+        }
+    }
+}
