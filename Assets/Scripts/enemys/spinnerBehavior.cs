08005adf91059a1b512ab18b0d0e19d2189ec776
Jason <4jasonmann@gmail.com>
2019-08-02 14:53:47 +0200

    spinner

--- Assets/Scripts/enemys/spinnerBehavior.cs
+++ Assets/Scripts/enemys/spinnerBehavior.cs
@@ -2,37 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class crobotBehavior : MonoBehaviour
+public class spinnerBehavior : MonoBehaviour
 {
     public float StopdistanceBTWcharacter;
     public float StartdistanceBTWcharacter;
     private Transform Target;
     public float speed;
+    public GameObject projectile;
+    public Transform projectileSpawn;
+    public float StartTimeBTW;
+    private float TimeBTW;
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
-        
         Target = GameObject.FindGameObjectWithTag("Player").transform;
-       
+        TimeBTW = StartTimeBTW;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (Vector2.Distance(transform.position, Target.position) >= StopdistanceBTWcharacter &&
             Vector2.Distance(transform.position, Target.position) <= StartdistanceBTWcharacter)
-        {
-            transform.position = Vector2.MoveTowards(transform.position,Target.position,speed*Time.deltaTime);
+        {//wärend er in range ist schießt er
+            if (TimeBTW <= 0) { shoot(); TimeBTW = StartTimeBTW; }
+
+            transform.position = Vector2.MoveTowards(transform.position, Target.position, speed * Time.deltaTime);
         }
+        //stehen bleiben
         else if (Vector2.Distance(transform.position, Target.position) <= StopdistanceBTWcharacter)
         {
+            if (TimeBTW <= 0) { shoot(); TimeBTW = StartTimeBTW; }
             transform.position = this.transform.position;
 
         }
-        else if (Vector2.Distance( transform.position, Target.position) <= StartdistanceBTWcharacter)
+        else if (Vector2.Distance(transform.position, Target.position) >= StartdistanceBTWcharacter)
         {
             transform.position = this.transform.position;
 
         }
+        TimeBTW -= Time.deltaTime;
+    
+}
+    void shoot()
+    {
+        Instantiate(projectile, projectileSpawn.position, projectileSpawn.rotation);
     }
 }
