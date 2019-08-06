fdc31bbdf76bd4e4e9835d9a78f4c984ae4b57a2
Jason <4jasonmann@gmail.com>
2019-08-06 13:59:16 +0200

    egger 1.1

--- Assets/Scripts/enemys/egger/shellPieces.cs
+++ Assets/Scripts/enemys/egger/shellPieces.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class shellPieces : MonoBehaviour
-{
+{ public ParticleSystem deathpart;
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 3f);
+        StartCoroutine(death());
     }
-
     // Update is called once per frame
     void Update()
     {
        
     }
+   public IEnumerator death()
+        {
+        yield return new WaitForSeconds(3f);
+        Instantiate(deathpart, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+        }
 }
